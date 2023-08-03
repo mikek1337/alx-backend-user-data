24e63d305e11cd0db7d643e1b566677fc04e80e6
Mikek1337 <mikekegnlyew@gmail.com>
2023-08-03 10:54:19 +0300

    added folder

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""obfuscate data from a log file"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: List[str], separator: str) -> List[str]:
+    """return the log message obfuscated"""
+    for i in fields:
+        message = re.sub(i + "=.*?" + separator,
+                         i + "=" + redaction + separator, message)
+    return message
