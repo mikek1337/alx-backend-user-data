b57bf310e685210d5a688c205685f5775b2b904d
mikek1337 <mikekegnlyew@gmail.com>
2023-08-12 14:21:39 +0300

    created 401 unauthorize error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,12 +14,6 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
-@app_views.route('unauthorized')
-def unauthorized():
-    """test for unauthorized endpoint"""
-    return abort(401)
-
-
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
