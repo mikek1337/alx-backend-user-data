acd34d77940b19e0dbda3a46b529ee84a681790d
mikek1337 <mikekegnlyew@gmail.com>
2023-08-17 21:27:32 +0300

    user login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask app module"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -30,6 +30,23 @@ def register_user() -> str:
         return jsonify({"email": new_user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "user already exists"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """POST /sessions
+    login user
+    return:
+      json payload"""
+    email = request.form['email']
+    password = request.form['password']
+    user_exists = AUTH.valid_login(email, password)
+    if user_exists:
+        session_id = AUTH.create_session(email)
+        app.config["session_id"] = session_id
+        return jsonify({"email": email, "message": "logged in"})
+    else:
+        return abort(401)
 
 
 if __name__ == "__main__":
