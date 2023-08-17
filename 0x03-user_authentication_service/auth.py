1596dbd96b6f75bb3d20b05c22c5ad14cf5d9cb1
Mikek1337 <mikekegnlyew@gmail.com>
2023-08-17 14:43:53 +0300

    hash password using bcrypt

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Auth module"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Takes in a password string arguments and returns bytes.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
